0524aa75dbea24032d3fa2d17dfce0d7b83e592c
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-11-19 08:07:17 -0300

    add search candidate feature

--- src/pages/TrashCan/index.tsx
+++ src/pages/TrashCan/index.tsx
@@ -27,6 +27,7 @@ interface Avatar {
 }
 
 export default function TrashCan() {
+  const [textInput, setTextInput] = useState('');
   const [avatarAccount, setAvatarAccount] = useState<Avatar[]>([]);
   const [candidates, setCandidates] = useState(allcandidates);
 
@@ -43,7 +44,11 @@ export default function TrashCan() {
           <nav>
             <img src={logo} alt="Company Logo" />
 
-            <input placeholder="Buscar" />
+            <input
+              value={textInput}
+              onChange={(event) => setTextInput(event.target.value)}
+              placeholder="Buscar"
+            />
 
             <span>
               {avatarAccount.map((item) => (
@@ -85,27 +90,56 @@ export default function TrashCan() {
         </aside>
 
         <section className="candidateslist">
-          {candidates.map((item) => (
-            <main key={item.login.uuid}>
-              <Link to="/candidate">
-                <div>
-                  <img src={item.picture.thumbnail} alt={item.name.first} />
-                  <h2>{item.name.first}</h2>
-                </div>
-              </Link>
-              <div>
-                <p>{item.email}</p>
+          {!textInput
+            ? candidates.map((item) => (
+                <main key={item.login.uuid}>
+                  <Link to="/candidate">
+                    <div>
+                      <img src={item.picture.thumbnail} alt={item.name.first} />
+                      <h2>{item.name.first}</h2>
+                    </div>
+                  </Link>
+                  <div>
+                    <p>{item.email}</p>
 
-                <p>{item.phone}</p>
+                    <p>{item.phone}</p>
 
-                <p>{item.location.city}</p>
-              </div>
-              <div>
-                <MdSelectAll size={26} color="#a9a9a9" />
-                <MdCheck size={26} color="#a9a9a9" />
-              </div>
-            </main>
-          ))}
+                    <p>{item.location.city}</p>
+                  </div>
+
+                  <div>
+                    <MdSelectAll size={26} color="#a9a9a9" />
+                    <MdCheck size={26} color="#a9a9a9" />
+                  </div>
+                </main>
+              ))
+            : candidates
+                .filter((candidate) => candidate.name.first === textInput)
+                .map((item) => (
+                  <main key={item.login.uuid}>
+                    <Link to="/candidate">
+                      <div>
+                        <img
+                          src={item.picture.thumbnail}
+                          alt={item.name.first}
+                        />
+                        <h2>{item.name.first}</h2>
+                      </div>
+                    </Link>
+                    <div>
+                      <p>{item.email}</p>
+
+                      <p>{item.phone}</p>
+
+                      <p>{item.location.city}</p>
+                    </div>
+
+                    <div>
+                      <MdSelectAll size={26} color="#a9a9a9" />
+                      <MdCheck size={26} color="#a9a9a9" />
+                    </div>
+                  </main>
+                ))}
         </section>
       </div>
     </>
