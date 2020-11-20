2a5dfd196d20f0c66803c1a649863e777586b607
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-11-20 13:50:16 -0300

    add skeleton effect

--- src/components/CandidateList/index.js
+++ src/components/CandidateList/index.js
@@ -1,35 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { MdSelectAll, MdDelete, MdCheck } from 'react-icons/md';
 
+import SkeletonEffect from '../../components/SkeletonEffect';
+
+import candidatesapi from '../../services/candidatesapi.json';
+
 export default function CandidateList({ textInput, candidates }) {
-  return (
-    <section className="candidateslist">
-      {!textInput
-        ? candidates.map((item) => (
-            <main key={item.login.uuid}>
-              <Link to={`/candidates/${item.login.uuid}`}>
-                <div>
-                  <img src={item.picture.thumbnail} alt={item.name.first} />
-                  <h2>{item.name.first}</h2>
-                </div>
-              </Link>
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
+  const candidateName = candidatesapi
+    .filter((filterName) => filterName.name.first === textInput)
+    .map((item) => item.name.first)
+    .toString();
+
+  if (isLoading) {
+    return (
+      <section className="candidateslist">
+        <SkeletonEffect />
+      </section>
+    );
+  } else if (!isLoading && !textInput) {
+    return (
+      <section className="candidateslist">
+        {candidates.map((item) => (
+          <main key={item.login.uuid}>
+            <Link to={`/candidates/${item.login.uuid}`}>
               <div>
-                <p>{item.email}</p>
-
-                <p>{item.phone}</p>
-
-                <p>{item.location.city}</p>
+                <img src={item.picture.thumbnail} alt={item.name.first} />
+                <h2>{item.name.first}</h2>
               </div>
+            </Link>
+            <div>
+              <p>{item.email}</p>
 
-              <div>
-                <MdDelete size={26} color="#a9a9a9" />
-                <MdSelectAll size={26} color="#a9a9a9" />
-                <MdCheck size={26} color="#a9a9a9" />
-              </div>
-            </main>
-          ))
-        : candidates
+              <p>{item.phone}</p>
+
+              <p>{item.location.city}</p>
+            </div>
+
+            <div>
+              <MdDelete size={26} color="#a9a9a9" />
+              <MdSelectAll size={26} color="#a9a9a9" />
+              <MdCheck size={26} color="#a9a9a9" />
+            </div>
+          </main>
+        ))}
+      </section>
+    );
+  } else {
+    return (
+      <section className="candidateslist">
+        {textInput !== candidateName ? (
+          <SkeletonEffect />
+        ) : (
+          candidates
             .filter((candidate) => candidate.name.first === textInput)
             .map((item) => (
               <main key={item.login.uuid}>
@@ -53,7 +84,9 @@ export default function CandidateList({ textInput, candidates }) {
                   <MdCheck size={26} color="#a9a9a9" />
                 </div>
               </main>
-            ))}
-    </section>
-  );
+            ))
+        )}
+      </section>
+    );
+  }
 }
