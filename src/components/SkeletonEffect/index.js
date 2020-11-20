a8453a2141f35d7f29757ca5290b0b87fc15fa19
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-11-20 15:40:43 -0300

    add more effects

--- src/components/SkeletonEffect/index.js
+++ src/components/SkeletonEffect/index.js
@@ -3,5 +3,5 @@ import React from 'react';
 import Skeleton from 'react-loading-skeleton';
 
 export default function SkeletonEffect() {
-  return <Skeleton count={6} height={70} />;
+  return <Skeleton count={8} height={70} />;
 }
