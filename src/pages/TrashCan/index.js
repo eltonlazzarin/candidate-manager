e9cb8b474d861f372975ed2ac3295a73d6eca05a
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-11-19 22:35:58 -0300

    add header and candidates list components

--- src/pages/TrashCan/index.js
+++ src/pages/TrashCan/index.js
@@ -1,67 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { MdSelectAll, MdDeleteSweep, MdCheck } from 'react-icons/md';
+import { MdSelectAll, MdDeleteSweep } from 'react-icons/md';
 import { BsCheckAll } from 'react-icons/bs';
-
-import logo from '../../assets/images/logo.png';
-
-import api from '../../services/api';
 
 import allcandidates from '../../services/candidatesdeleted.json';
 
-interface Avatar {
-  email: string;
-  phone: string;
-  picture: {
-    thumbnail: string;
-  };
-  login: {
-    uuid: string;
-  };
-  name: {
-    first: string;
-  };
-  location: {
-    city: string;
-  };
-}
+import CandidateList from '../../components/CandidateList';
+import Header from '../../components/Header';
 
 export default function TrashCan() {
   const [textInput, setTextInput] = useState('');
-  const [avatarAccount, setAvatarAccount] = useState<Avatar[]>([]);
   const [candidates, setCandidates] = useState(allcandidates);
-
-  useEffect(() => {
-    api.get('api').then((response) => {
-      setAvatarAccount(response.data.results);
-    });
-  }, []);
 
   return (
     <>
-      <div className="page-allcadidadtes">
-        <header>
-          <nav>
-            <img src={logo} alt="Company Logo" />
-
-            <input
-              value={textInput}
-              onChange={(event) => setTextInput(event.target.value)}
-              placeholder="Buscar"
-            />
-
-            <span>
-              {avatarAccount.map((item) => (
-                <img
-                  key={item.login.uuid}
-                  src={item.picture.thumbnail}
-                  alt={item.name.first}
-                />
-              ))}
-            </span>
-          </nav>
-        </header>
-      </div>
+      <Header textInput={textInput} setTextInput={setTextInput} />
 
       <div className="navbar">
         <aside>
@@ -89,58 +42,7 @@ export default function TrashCan() {
           </ul>
         </aside>
 
-        <section className="candidateslist">
-          {!textInput
-            ? candidates.map((item) => (
-                <main key={item.login.uuid}>
-                  <Link to="/candidate">
-                    <div>
-                      <img src={item.picture.thumbnail} alt={item.name.first} />
-                      <h2>{item.name.first}</h2>
-                    </div>
-                  </Link>
-                  <div>
-                    <p>{item.email}</p>
-
-                    <p>{item.phone}</p>
-
-                    <p>{item.location.city}</p>
-                  </div>
-
-                  <div>
-                    <MdSelectAll size={26} color="#a9a9a9" />
-                    <MdCheck size={26} color="#a9a9a9" />
-                  </div>
-                </main>
-              ))
-            : candidates
-                .filter((candidate) => candidate.name.first === textInput)
-                .map((item) => (
-                  <main key={item.login.uuid}>
-                    <Link to="/candidate">
-                      <div>
-                        <img
-                          src={item.picture.thumbnail}
-                          alt={item.name.first}
-                        />
-                        <h2>{item.name.first}</h2>
-                      </div>
-                    </Link>
-                    <div>
-                      <p>{item.email}</p>
-
-                      <p>{item.phone}</p>
-
-                      <p>{item.location.city}</p>
-                    </div>
-
-                    <div>
-                      <MdSelectAll size={26} color="#a9a9a9" />
-                      <MdCheck size={26} color="#a9a9a9" />
-                    </div>
-                  </main>
-                ))}
-        </section>
+        <CandidateList textInput={textInput} candidates={candidates} />
       </div>
     </>
   );
